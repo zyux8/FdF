f42bf87c3d2dfce4cff3c06f2f6ef81dd7be74ea
zyux8 <hakerole@gmail.com>
2025-05-20 23:09:50 +0200

    [FINISHED]

--- src/handler.c
+++ src/handler.c
@@ -6,7 +6,7 @@
 /*   By: ohaker <ohaker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 21:19:04 by ohaker            #+#    #+#             */
-/*   Updated: 2025/05/12 18:50:24 by ohaker           ###   ########.fr       */
+/*   Updated: 2025/05/20 21:41:34 by ohaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 int	handle_destroy(t_data *data)
 {
-	// mlx_destroy_image(data->mlx, data->img);
-	// mlx_destroy_window(data->mlx, data->win);
-	// mlx_destroy_display(data->mlx);
-	// free(data->mlx);
-	// exit(0);
 	cleanup_and_exit(data);
 	return (0);
 }
@@ -26,7 +21,10 @@ int	handle_destroy(t_data *data)
 int	handle_zoom(int keycode, t_data *data)
 {
 	if (keycode == KEY_J)
-		data->map->scale += 1;
+	{
+		if (data->map->scale < 20)
+			data->map->scale += 1;
+	}
 	else if (keycode == KEY_K)
 	{
 		if (data->map->scale > 1)
@@ -39,7 +37,10 @@ int	handle_zoom(int keycode, t_data *data)
 int	handle_z(int keycode, t_data *data)
 {
 	if (keycode == KEY_I)
-		data->map->z_scale += 1;
+	{
+		if (data->map->z_scale < 20)
+			data->map->z_scale += 1;
+	}
 	else if (keycode == KEY_O)
 	{
 		if (data->map->z_scale > 1)
